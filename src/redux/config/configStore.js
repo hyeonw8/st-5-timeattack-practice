e5e39a33f4e601f89176154195cf4a9aeae7061f
hyeonw8 <whgusrud223@gmail.com>
2024-06-10 19:34:46 +0900

    🎉 add : 타임어택 복습 calculator 초기 파일 생성

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,1 @@
-// TODO: store 셋업하고 slice에서 import한 reducer를 할당하세요.
-import { configureStore } from '@reduxjs/toolkit';
-import todoSlice from '../slices/todoSlice';
-
-const store = configureStore({
-  reducer: {
-    todos: todoSlice
-  }
-})
-
-export default store;
+// TODO: result 상태와 덧셈, 뺄셈에 대한 상태변경 로직을 담은 slice를 만들어 보세요.
